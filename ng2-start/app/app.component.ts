0f76772a592bd3a8ae94e8dcbce5572342a7f76d
Garcia Marchena <alejandro.garciamarchena.external@atos.net>
2017-05-15 17:10:17 +0200

    initial Angular2 app

---
+++ ng2-start/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';    
+
+@Component({
+    selector:'my-app',
+    template:`
+    <div class="jumbotron">
+        <h1>Welcome to my App!!</h1>
+    </div>
+    `
+})
+
+export class AppComponent{
+
+}
